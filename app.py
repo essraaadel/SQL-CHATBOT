679f4a0debf3ae2f9515eafc0d3901de84139d32
Essraa <161943448+essraaadel@users.noreply.github.com>
2026-02-22 03:16:24 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from sqlalchemy import create_engine, text
 from langchain_google_genai import GoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.documents import Document
 import re
@@ -58,9 +59,8 @@ def get_llm():
 
 @st.cache_resource
 def get_embeddings():
-    return GoogleGenerativeAIEmbeddings(
-        model="models/embedding-001",
-        google_api_key=GOOGLE_API_KEY
+    return HuggingFaceEmbeddings(
+        model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
     )
 
 llm = get_llm()
